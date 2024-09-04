99f0773d7851a5021b2f282c187ac4a29e314f9c
isconnei <icontrerasn@pm.me>
2024-09-04 01:47:28 -0400

    Lista 2.3 y lista para revisión final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
+///////////////////////////////////////////
+//************** Punto 2.2 *************//
+/////////////////////////////////////////
+//Nota: Se comenta para hacer punto 2.3
+
+// const ele = document.getElementById("ele1");
+
+// ele.addEventListener(
+//   "click",
+//   (pintar = () => (ele.style.backgroundColor = "yellow"))
+// );
+
+///////////////////////////////////////////
+//************** Punto 2.3 *************//
+/////////////////////////////////////////
 const ele = document.getElementById("ele1");
 
-ele.addEventListener("click", function (color = "green") {
-  ele.style.backgroundColor = "yellow";
-});
+const pintar = (color = "green") => (ele.style.backgroundColor = color);
+ele.addEventListener("click", () => pintar("yellow"));
